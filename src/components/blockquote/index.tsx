a800775442c3e1eaeb4e1579d8468336af818d15
agun36 <aguntoroti10@gmail.com>
2024-06-15 16:29:17 +0100

    modified: blockquote

--- src/components/blockquote/index.tsx
+++ src/components/blockquote/index.tsx
@@ -1,6 +1,6 @@
 import './styles.scss'
 export const Blockquote = () => {
-    return <blockquote className='bg-bg-300 text-center  '>
+    return <blockquote className='bg-bg-300 text-center row  '>
         Filetra Enterprise Document Management System for government and business organizations provides an efficient and effective way to manage documents and streamline business processes by eliminating paper-based documents, reducing storage space requirements, and increasing document security. It includes document storage and organization, document retrieval, version control, access control and security, document collaboration, workflow automation, audit trail, and system integration. Filetra is a product of Rongil.
 
     </blockquote>
