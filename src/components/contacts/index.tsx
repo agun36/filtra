652b93f3477c3796383302c46fb799d1645315d7
agun36 <aguntoroti10@gmail.com>
2024-06-15 16:52:34 +0100

    modified: contacts

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -8,7 +8,7 @@ export const Contacts = () => {
     return (
         <section className="contacts bg-bg-100 d-flex flex-column justify-content-center align-items-center modal-dialog modal-dialog-centered container-fluid">
             <div className="modal-content d-flex align-items-center flex-column">
-                <h1 className="text-white col-7 mb-5 text-center">Ready to simplify your workflow? Get started with Filetra today.</h1>
+                <h1 className="text-white col mb-5 text-center">Ready to simplify your workflow? Get started with Filetra today.</h1>
                 <button type='button' className="btn bg-white text-bg-100 px-6 py-3 opacity  " data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => setModalShow(true)}>Get Started</button>
             </div>
             {modalShow && <ContactModal show={modalShow} onHide={() => setModalShow(false)} labels={''} placefield={''} inputType={'input'} name={''} />}
