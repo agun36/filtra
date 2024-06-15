75f33073b01a1f82edaeb7b898c6ba95167e1628
agun36 <aguntoroti10@gmail.com>
2024-06-15 16:17:37 +0100

    modified: contacts

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -6,7 +6,7 @@ import { ContactModal } from '../hero/contact-modal';
 export const Contacts = () => {
     const [modalShow, setModalShow] = useState(false);
     return (
-        <section className="contacts bg-bg-100 d-flex flex-column justify-content-center align-items-center modal-dialog modal-dialog-centered">
+        <section className="contacts bg-bg-100 d-flex flex-column justify-content-center align-items-center modal-dialog modal-dialog-centered container-fluid">
             <div className="modal-content d-flex align-items-center flex-column">
                 <h1 className="text-white col mb-5 text-center">Ready to simplify your workflow? Get started with Filetra today.</h1>
                 <button type='button' className="btn bg-white text-bg-100 px-6 py-3 opacity  " data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => setModalShow(true)}>Get Started</button>
