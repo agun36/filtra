194833aca4de16741092335322753fd6524ac2fd
agun36 <aguntoroti10@gmail.com>
2024-06-18 11:27:58 +0100

    modified: blockqoute

--- src/components/blockquote/index.tsx
+++ src/components/blockquote/index.tsx
@@ -1,7 +1,9 @@
 import './styles.scss'
 export const Blockquote = () => {
-    return <blockquote className='bg-bg-300 text-center row  '>
-        Filetra Enterprise Document Management System for government and business organizations provides an efficient and effective way to manage documents and streamline business processes by eliminating paper-based documents, reducing storage space requirements, and increasing document security. It includes document storage and organization, document retrieval, version control, access control and security, document collaboration, workflow automation, audit trail, and system integration. Filetra is a product of Rongil.
+    return <blockquote className=' row  bg-bg-300  '>
+        <p className='text-center col-8 mx-auto'>
+            Filetra Enterprise Document Management System for government and business organizations provides an efficient and effective way to manage documents and streamline business processes by eliminating paper-based documents, reducing storage space requirements, and increasing document security. It includes document storage and organization, document retrieval, version control, access control and security, document collaboration, workflow automation, audit trail, and system integration. Filetra is a product of Rongil.
+        </p>
 
     </blockquote>
 }
